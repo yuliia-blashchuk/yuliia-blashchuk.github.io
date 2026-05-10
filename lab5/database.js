12c196563c8aad9656872174459ebb0c9474bbab
Yuliia Blashchuk <yulyusia7@gmail.com>
2026-05-10 18:22:33 +0300

    fix: remove dotenv from database

--- lab5/database.js
+++ lab5/database.js
@@ -1,9 +1,7 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  protocol: 'postgres',
   dialectOptions: {
     ssl: {
       require: true,
