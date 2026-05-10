30bc3b6bd5c82cf644b8f784246ac5c1017b38d6
Yuliia Blashchuk <yulyusia7@gmail.com>
2026-05-10 18:15:39 +0300

    fix database connection

--- lab5/database.js
+++ lab5/database.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  protocol: 'postgres',
   dialectOptions: {
     ssl: {
       require: true,
